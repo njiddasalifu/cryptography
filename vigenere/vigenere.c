93e148ab79aa58b24cfc89643e452f10ee0ad601
njiddasalifu <njiddasalifu@gmail.com>
2023-04-27 01:05:11 +0100

    defining the caeser concepts

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -1,3 +1,5 @@
 // caeser cypher is a substitution cypher
 // caeser algorithm:  For any plaintext letter 'a', substitue
-// the ciphertext letter 'B'
+// the ciphertext letter 'B' ~
+// formular B = E(a, k)mod26 = (a + k)mod26
+// k =  key 
